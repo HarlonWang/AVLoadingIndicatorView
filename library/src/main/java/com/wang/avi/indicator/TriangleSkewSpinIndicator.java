7603a78c609d8408e0c6480d964386fecd4adf69
selmanon <selassi.abdellah@gmail.com>
2015-10-31 01:02:42 +0000

    remove commented code

--- library/src/main/java/com/wang/avi/indicator/TriangleSkewSpinIndicator.java
+++ library/src/main/java/com/wang/avi/indicator/TriangleSkewSpinIndicator.java
@@ -25,38 +25,9 @@ public class TriangleSkewSpinIndicator extends BaseIndicatorController {
 
     @Override
     public void createAnimation() {
-        /*ObjectAnimator rotationXAnimation=ObjectAnimator.ofFloat(getTarget(), "rotationX",0,180,180,0,0);
-        rotationXAnimation.setDuration(3000);
-
-        ObjectAnimator rotationYAnimation=ObjectAnimator.ofFloat(getTarget(), "rotationY",0,0,180,180,0);
-        rotationYAnimation.setDuration(3000);
-
-        final AnimatorSet animatorSet=new AnimatorSet();
-        animatorSet.playTogether(rotationXAnimation,rotationYAnimation);
-        animatorSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                super.onAnimationRepeat(animation);
-                System.out.println("onAnimationRepeat");
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                System.out.println("onAnimationEnd(");
-                animatorSet.start();
-            }
-        });
-        animatorSet.start();*/
-
-
-        /*PropertyValuesHolder rotation1=PropertyValuesHolder.ofFloat("rotationX",0,180,0,0,0);
-        PropertyValuesHolder rotation2=PropertyValuesHolder.ofFloat("rotationX",0,0,180,0,0);
-        PropertyValuesHolder rotation3=PropertyValuesHolder.ofFloat("rotationY",0,0,180,0,0);
-        PropertyValuesHolder rotation4=PropertyValuesHolder.ofFloat("rotationY",0,0,0,180,0);*/
-
         PropertyValuesHolder rotation5=PropertyValuesHolder.ofFloat("rotationX",0,180,180,0,0);
         PropertyValuesHolder rotation6=PropertyValuesHolder.ofFloat("rotationY",0,0,180,180,0);
+        
         ObjectAnimator animator=ObjectAnimator.ofPropertyValuesHolder(getTarget(), rotation6,rotation5);
         animator.setInterpolator(new LinearInterpolator());
         animator.setRepeatCount(-1);
