47c9eae47a66a3937435aa63868da6719b7b8ea2
whlong <81813780@qq.com>
2016-08-06 11:41:37 +0800

    Add hide and show way

--- library/src/main/java/com/wang/avi/AVLoadingIndicatorView.java
+++ library/src/main/java/com/wang/avi/AVLoadingIndicatorView.java
@@ -10,7 +10,6 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v4.widget.ContentLoadingProgressBar;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
