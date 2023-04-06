f655cc564fd6f5f5eab27487a1138a3d547565f3
Syed Raza Mehdi <raza.mehdi@pitb.gov.pk>
2023-04-06 11:03:44 +0500

    gradle dependencies and code revived

--- app/src/main/java/com/wang/avi/sample/MyCustomIndicator.java
+++ app/src/main/java/com/wang/avi/sample/MyCustomIndicator.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 
 import android.animation.ValueAnimator;
+
 import com.wang.avi.Indicator;
 
 import java.util.ArrayList;
@@ -12,29 +13,26 @@ import java.util.ArrayList;
  * Created by Jack Wang on 2016/8/5.
  */
 
-public class MyCustomIndicator extends Indicator{
-
-
-    public static final float SCALE=1.0f;
+public class MyCustomIndicator extends Indicator {
+    public static final float SCALE = 1.0f;
 
     //scale x ,y
-    private float[] scaleFloats=new float[]{SCALE,
+    private float[] scaleFloats = new float[]{SCALE,
             SCALE,
             SCALE,
             SCALE,
             SCALE};
 
 
-
     @Override
     public void draw(Canvas canvas, Paint paint) {
-        float circleSpacing=4;
-        float radius=(Math.min(getWidth(),getHeight())-circleSpacing*2)/12;
-        float x = getWidth()/ 2-(radius*2+circleSpacing);
-        float y=getHeight() / 2;
+        float circleSpacing = 4;
+        float radius = (Math.min(getWidth(), getHeight()) - circleSpacing * 2) / 12;
+        float x = getWidth() / 2 - (radius * 2 + circleSpacing);
+        float y = getHeight() / 2;
         for (int i = 0; i < 4; i++) {
             canvas.save();
-            float translateX=x+(radius*2)*i+circleSpacing*i;
+            float translateX = x + (radius * 2) * i + circleSpacing * i;
             canvas.translate(translateX, y);
             canvas.scale(scaleFloats[i], scaleFloats[i]);
             canvas.drawCircle(0, 0, radius, paint);
@@ -44,18 +42,18 @@ public class MyCustomIndicator extends Indicator{
 
     @Override
     public ArrayList<ValueAnimator> onCreateAnimators() {
-        ArrayList<ValueAnimator> animators=new ArrayList<>();
-        int[] delays=new int[]{120,240,360,480};
+        ArrayList<ValueAnimator> animators = new ArrayList<>();
+        int[] delays = new int[]{120, 240, 360, 480};
         for (int i = 0; i < 4; i++) {
-            final int index=i;
+            final int index = i;
 
-            ValueAnimator scaleAnim=ValueAnimator.ofFloat(1,0.3f,1);
+            ValueAnimator scaleAnim = ValueAnimator.ofFloat(1, 0.3f, 1);
 
             scaleAnim.setDuration(750);
             scaleAnim.setRepeatCount(-1);
             scaleAnim.setStartDelay(delays[i]);
 
-            addUpdateListener(scaleAnim,new ValueAnimator.AnimatorUpdateListener() {
+            addUpdateListener(scaleAnim, new ValueAnimator.AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator animation) {
                     scaleFloats[index] = (float) animation.getAnimatedValue();
